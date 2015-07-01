23e38fa34c31d6f71b5e8d0499bf3b5aa5446884
Vanova <iva427@mail.ru>
2015-07-01 16:49:25 +0300

    Add simple test client to Percolation for union-find

--- trunk/algorithms/src/org.uef.coursera.algfirst.percolation/Percolation.java
+++ trunk/algorithms/src/org.uef.coursera.algfirst.percolation/Percolation.java
@@ -1,4 +1,8 @@
-package org.uef.coursera.algfirst.hw_1;
+package org.uef.coursera.algfirst.percolation;
+
+import edu.princeton.cs.algs4.UF;
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
 
 public class Percolation {
 
@@ -29,6 +33,17 @@ public class Percolation {
 
     // test client (optional)
     public static void main(String[] args) {
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
 
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
+            }
+        }
     }
 }
